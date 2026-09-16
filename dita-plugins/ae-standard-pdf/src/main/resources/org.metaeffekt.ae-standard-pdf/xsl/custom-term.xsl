61b42742d9d87e147195c74014ef0cc8bb477ca7
karsten-klein <karsten.klein@metaeffekt.com>
2026-09-16 16:56:22 +0200

    AE-2374: review / tests

--- dita-plugins/ae-standard-pdf/src/main/resources/org.metaeffekt.ae-standard-pdf/xsl/custom-term.xsl
+++ dita-plugins/ae-standard-pdf/src/main/resources/org.metaeffekt.ae-standard-pdf/xsl/custom-term.xsl
@@ -40,5 +40,4 @@
         </xsl:choose>
     </xsl:template>
 
-
 </xsl:stylesheet>
--- maven-plugins/ae-dita-maven-plugin/src/main/java/org/metaeffekt/dita/maven/glossary/GlossaryMapCreator.java
+++ maven-plugins/ae-dita-maven-plugin/src/main/java/org/metaeffekt/dita/maven/glossary/GlossaryMapCreator.java
@@ -253,7 +253,6 @@ public class GlossaryMapCreator {
         for (Node node : selectedNodes) {
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 String keyRef = ((Element) node).attributeValue("keyref");
-                if (keyRef == null) keyRef = ((Element) node).attributeValue("xref");
                 if (keyRef != null && !keyRefs.contains(keyRef)) {
                     keyRefs.add(keyRef);
                 }
